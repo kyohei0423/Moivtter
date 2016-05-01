7b850097d1be0f1c8c704128b6074a69ba319b45
Kyohei Seo <kyo199333@gmail.com>
2016-05-01 19:43:34 +0900

    Add view controller and view to show movies
    add MovieListViewController
    add MovieListView, include xib file

---
+++ Movitter/Movitter/MovieListView.swift
@@ -0,0 +1,21 @@
+//
+//  MovieListView.swift
+//  Movitter
+//
+//  Created by Seo Kyohei on 2016/05/01.
+//  Copyright © 2016年 Kyohei Seo. All rights reserved.
+//
+
+import UIKit
+
+class MovieListView: UIView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
