749eb80cbfbf227235b49e0269b72c65f0094de5
Kyohei Seo <kyo199333@gmail.com>
2016-05-05 12:55:10 +0900

    Add files of ViewController and View

---
+++ Movitter/Movitter/TakeMovieView.swift
@@ -0,0 +1,21 @@
+//
+//  TakeMovieView.swift
+//  Movitter
+//
+//  Created by Seo Kyohei on 2016/05/05.
+//  Copyright © 2016年 Kyohei Seo. All rights reserved.
+//
+
+import UIKit
+
+class TakeMovieView: UIView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
