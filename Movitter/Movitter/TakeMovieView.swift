b0c37923b065153f43c96a6058d97d1083a4c28e
Kyohei Seo <kyo199333@gmail.com>
2016-05-09 02:54:09 +0900

    Layout TakeMovieView
    - add image
    - set outlets
    - add constraints for auto layout

--- Movitter/Movitter/TakeMovieView.swift
+++ Movitter/Movitter/TakeMovieView.swift
@@ -7,9 +7,19 @@
 //
 
 import UIKit
+import AVFoundation
 
 class TakeMovieView: UIView {
 
+    @IBOutlet weak var captureView: UIView!
+    @IBOutlet weak var recordButton: UIButton!
+    
+    func drawCaptureView(session: AVCaptureSession) {
+        let videoPreview = AVCaptureVideoPreviewLayer(session: session)
+        videoPreview.frame = captureView.bounds
+        videoPreview.videoGravity = AVLayerVideoGravityResizeAspectFill
+        captureView.layer.addSublayer(videoPreview)
+    }
     /*
     // Only override drawRect: if you perform custom drawing.
     // An empty implementation adversely affects performance during animation.
