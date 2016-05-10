bcbe7c89a48643f3405ce8cc93e889936f76b082
Kyohei Seo <kyo199333@gmail.com>
2016-05-10 20:43:22 +0900

    Add function to take movie

---
+++ Movitter/Movitter/MovieManager.swift
@@ -0,0 +1,95 @@
+//
+//  MovieManager.swift
+//  Movitter
+//
+//  Created by Seo Kyohei on 2016/05/05.
+//  Copyright © 2016年 Kyohei Seo. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+import Photos
+
+class MovieManager: NSObject {
+    //データの入力と出力を管理する
+    var session: AVCaptureSession?
+    //使用するデバイスを指定（フロントカメラ・バックカメラ）
+    var device: AVCaptureDevice?
+    //データの入力を行う
+    var input: AVCaptureInput?
+    //データの出力を行う
+    var output: AVCaptureMovieFileOutput?
+    //レコード中かどうか
+    var isRecording = false
+
+    var sessionQueue: dispatch_queue_t?
+
+    //カメラロールへのアクセスの許可の有無を確認する
+    func checkAuthorization(callback: Bool -> ()) {
+        switch AVCaptureDevice.authorizationStatusForMediaType(AVMediaTypeVideo) {
+        case .Authorized:   //許可
+            callback(true)
+        case .Denied, .Restricted:  //アプリ自体が不許可の場合、ユーザーによって不許可にされた場合
+            callback(false)
+        case .NotDetermined:    //どちらも選択していない場合
+            requestAccessToCamera()
+        }
+    }
+
+    func begin() {
+        session?.startRunning()
+    }
+
+    func stop() {
+        session?.stopRunning()
+    }
+    
+    func finishRecord(outputFile: NSURL) {
+        PHPhotoLibrary.sharedPhotoLibrary().performChanges({ () -> Void in
+            let list = PHAssetCollection.fetchAssetCollectionsWithType(PHAssetCollectionType.Album, subtype: PHAssetCollectionSubtype.Any, options: nil)
+            var assetAlbum = PHAssetCollection()
+            
+            list.enumerateObjectsUsingBlock { (album, index, isStop) -> Void in
+                if album.localizedTitle == "video" {
+                    assetAlbum = album as! PHAssetCollection
+                    isStop.memory = true
+                }
+            }
+            let result = PHAssetChangeRequest.creationRequestForAssetFromVideoAtFileURL(outputFile)
+            let assetPlaceholder = result?.placeholderForCreatedAsset
+            let saveAlbum = PHAssetCollectionChangeRequest(forAssetCollection: assetAlbum)
+            saveAlbum?.addAssets([assetPlaceholder] as! NSFastEnumeration)
+        }) { (success, error) -> Void in
+            if success {
+                print("success")
+            } else {
+                print(error)
+            }
+        }
+    }
+
+
+    private func requestAccessToCamera() {
+        AVCaptureDevice.requestAccessForMediaType(AVMediaTypeVideo, completionHandler: { (success) -> Void in
+            self.setUPCamera()
+        })
+    }
+
+    func setUPCamera() {
+        device = AVCaptureDevice.defaultDeviceWithMediaType(AVMediaTypeVideo)
+        do {
+            session = AVCaptureSession()
+            if let session = session {
+                session.beginConfiguration()
+                input = try AVCaptureDeviceInput.init(device: device)
+                session.addInput(input)
+                output = AVCaptureMovieFileOutput()
+                session.addOutput(output)
+                session.commitConfiguration()
+            }
+        } catch let error as NSError {
+            print(error)
+            session = nil
+        }
+    }
+}
--- Movitter/Movitter/TakeMovieViewController.swift
+++ Movitter/Movitter/TakeMovieViewController.swift
@@ -7,18 +7,91 @@
 //
 
 import UIKit
+import AVFoundation
+import Photos
 
-class TakeMovieViewController: UIViewController {
+class TakeMovieViewController: UIViewController, AVCaptureFileOutputRecordingDelegate {
+    var movieManager: MovieManager!
+    var isRecording: Bool!
+    
+    override func loadView() {
+        super.loadView()
+        view = UINib.instantiateFirstView("TakeMovieView")
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        let takeMovieView = view as! TakeMovieView
+        takeMovieView.recordButton.addTarget(self, action: #selector(TakeMovieViewController.tapRecordButton), forControlEvents: .TouchUpInside)
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        isRecording = false
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+        movieManager = MovieManager()
+        movieManager.checkAuthorization(setUpCameraOrShowAlert)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func setUpCameraOrShowAlert(result: Bool) {
+        if result {
+            let takeMovieView = view as! TakeMovieView
+            movieManager.setUPCamera()
+            if let session = movieManager.session {
+                takeMovieView.drawCaptureView(session)
+                movieManager.begin()
+            }
+        } else {
+            moveSettingApp()
+        }
+    }
+    
+    func tapRecordButton() {
+        let takeMovieView = view as! TakeMovieView
+        guard let output = movieManager.output else { return }
+        if !isRecording {
+            let path = NSSearchPathForDirectoriesInDomains(.DocumentDirectory, .UserDomainMask, true)
+            let documentsDirectory = path[0]
+            let filePath: String = "\(documentsDirectory)/temp.mp4"
+            let fileURL: NSURL = NSURL(fileURLWithPath: filePath)
+            output.startRecordingToOutputFileURL(fileURL, recordingDelegate: self)
+            //録画中にする
+            isRecording = true
+            //ボタンの画像を停止画像にする
+            takeMovieView.recordButton.setImage(UIImage(named: "ShutterButtonStop"), forState: .Normal)
+        } else {
+            //録画を停止する
+            output.stopRecording()
+            //録画中でなくする
+            isRecording = false
+            //ボタンの状態を変更する
+            takeMovieView.recordButton.setImage(UIImage(named: "ShutterButton1"), forState: .Normal)
+        }
+    }
+    
+    func captureOutput(captureOutput: AVCaptureFileOutput!, didFinishRecordingToOutputFileAtURL outputFileURL: NSURL!, fromConnections connections: [AnyObject]!, error: NSError!) {
+        movieManager.finishRecord(outputFileURL)
+    }
+    
+    
+    private func moveSettingApp() {
+        let alertController = UIAlertController(title: "カメラへのアクセスが許可されていません", message: "設定アプリからカメラへのアクセスを許可してください", preferredStyle: .Alert)
+        let action = UIAlertAction(title: "設定する", style: .Default) { (_) in
+            let settingURL = NSURL(string: UIApplicationOpenSettingsURLString)
+            if let settingURL = settingURL {
+                UIApplication.sharedApplication().openURL(settingURL)
+            }
+        }
+        alertController.addAction(action)
+        presentViewController(alertController, animated: true, completion: nil)
     }
     
 
