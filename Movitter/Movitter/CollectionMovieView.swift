5b91412f6e9eba14bcdae974806a999ccdfe80c6
Kyohei Seo <kyo199333@gmail.com>
2016-05-16 20:11:46 +0900

    Add new files

---
+++ Movitter/Movitter/CollectionMovieView.swift
@@ -0,0 +1,21 @@
+//
+//  CollectionMovieView.swift
+//  Movitter
+//
+//  Created by Seo Kyohei on 2016/05/16.
+//  Copyright © 2016年 Kyohei Seo. All rights reserved.
+//
+
+import UIKit
+
+class CollectionMovieView: UIView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ Movitter/Movitter/CollectionMovieViewController.swift
@@ -0,0 +1,40 @@
+//
+//  CollectionMovieViewController.swift
+//  Movitter
+//
+//  Created by Seo Kyohei on 2016/05/16.
+//  Copyright © 2016年 Kyohei Seo. All rights reserved.
+//
+
+import UIKit
+
+class CollectionMovieViewController: UIViewController {
+    
+    override func loadView() {
+        super.loadView()
+        view = UINib.instantiateFirstView("CollectionMovieView")
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
