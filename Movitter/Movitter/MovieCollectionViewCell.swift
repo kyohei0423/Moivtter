4bd527fb5455add27b0a1dc0b5b631ad5d021849
Seo Kyohei <kyo199333@gmail.com>
2016-05-24 20:56:06 +0900

    Change code that display capture image

--- Movitter/Movitter/MovieCollectionViewCell.swift
+++ Movitter/Movitter/MovieCollectionViewCell.swift
@@ -19,13 +19,20 @@ class MovieCollectionViewCell: UICollectionViewCell {
     }
     
     func fillWith(asset: AVAsset) {
+//        let imageGenerator = AVAssetImageGenerator(asset: asset)
+//        let capturePoint = CMTimeMakeWithSeconds(0.0, 600)
+//        do {
+//            let captureImage = try imageGenerator.copyCGImageAtTime(capturePoint, actualTime: nil)
+//            captureImageView.image = UIImage(CGImage: captureImage)
+//        } catch let error {
+//            print(error)
+//        }
         let imageGenerator = AVAssetImageGenerator(asset: asset)
-        let capturePoint = CMTimeMakeWithSeconds(0.0, 600)
-        do {
-            let captureImage = try imageGenerator.copyCGImageAtTime(capturePoint, actualTime: nil)
-            captureImageView.image = UIImage(CGImage: captureImage)
-        } catch let error {
-            print(error)
+        let tVal = NSValue(CMTime: CMTimeMake(Int64(0.0), 1))
+        
+        imageGenerator.generateCGImagesAsynchronouslyForTimes([tVal]) { (_, image, _, _, _) in
+            guard let image = image else { return }
+            self.captureImageView.image = UIImage(CGImage: image)
         }
     }
     
