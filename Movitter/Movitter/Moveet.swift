7c9d9af50bd5a593153eb7db1b2df11929436a9e
Seo Kyohei <kyo199333@gmail.com>
2016-05-28 22:40:32 +0900

    Add model

---
+++ Movitter/Movitter/Moveet.swift
@@ -0,0 +1,13 @@
+//
+//  Moveet.swift
+//  Movitter
+//
+//  Created by Seo Kyohei on 2016/05/28.
+//  Copyright © 2016年 Kyohei Seo. All rights reserved.
+//
+
+import UIKit
+
+class Moveet: NSObject {
+
+}
